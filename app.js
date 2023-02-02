7e885199d74c86af4c539cba83f40d48dee98911
Tweety80 <122665378+Tweety80@users.noreply.github.com>
2023-02-02 20:45:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 (function functionName() {
         'use strict'; //wymusza koniecznośc deklarowania (typowania) zmiennych
 
-        var url ="http://10.56.123.135/asix/v1/variable/archive/raw?name=JO976KGW_OPC_PALA>
-
+        //var url ="http://10.56.123.135/asix/v1/variable/archive/raw?name=JO976KGW_OPC_PALA>
+        var url="ASIX_data.json"
         app = angular.module('myApp', [])
 
                 .controller('customersCtrl', function($scope, $http, $interval) {
